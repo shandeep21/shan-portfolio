797034ba7961a265fadeed489112bad150d56c3e
shandeep21 <shandeepsamy@gmail.com>
2023-11-15 12:09:34 +0400

    shan-portfolio-basic

--- postcss.config.js
+++ postcss.config.js
@@ -3,4 +3,8 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+  entry: {
+    // Other entry points...
+    client: "./src/app/page.js", // Mark 'page.js' as a client entry
+  },
+};
